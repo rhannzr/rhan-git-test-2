eea84c491a42d639f884a2a22fd56b42884c0471
rhannzr <hannizar12@gmail.com>
2024-07-08 11:00:02 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae ducimus laborum enim quo praesentium voluptas! Sunt facilis incidunt at quam laboriosam quaerat, molestiae vitae. Dignissimos molestiae consectetur natus vel reiciendis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
